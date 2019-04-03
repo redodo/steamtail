5366b57f0484677475d4c2c3651e2b8249b0fd01
redodo <me@redodo.io>
2019-04-03 11:33:38 +0200

    Fix static rootg

--- proj/settings.py
+++ proj/settings.py
@@ -97,3 +97,4 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/stable/howto/static-files/
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
