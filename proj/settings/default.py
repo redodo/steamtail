80009cdd2e644ee01abb92f47b2543ef148717b9
redodo <me@redodo.io>
2019-04-05 12:54:15 +0200

    Add support for DigitalOcean Spaces

--- proj/settings/default.py
+++ proj/settings/default.py
@@ -1,4 +1,5 @@
 import os
+from ..utils import secret_from_env
 
 from ._shared import *
 
@@ -27,6 +28,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'steamtail',
+
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -41,3 +44,28 @@ MIDDLEWARE = [
 
 WSGI_APPLICATION = 'proj.wsgi.application'
 ROOT_URLCONF = 'proj.urls'
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/stable/howto/static-files/
+if 'USE_S3_STATICFILES' in os.environ:
+    # Generic config for S3-like storages
+    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+    STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+    # DigitalOcean-specific config
+
+    # TODO: make these configurable
+    AWS_S3_REGION_NAME = 'ams3'
+    AWS_STORAGE_BUCKET_NAME = 'steamtail'
+
+    AWS_S3_ENDPOINT_URL = '${AWS_S3_REGION_NAME}.digitaloceanspaces.com'
+    AWS_ACCESS_KEY_ID = secret_from_env('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = secret_from_env('AWS_SECRET_ACCESS_KEY')
+
+    STATIC_URL = 'https://%s.%s/' % (
+        AWS_STORAGE_BUCKET_NAME,
+        AWS_S3_ENDPOINT_URL,
+    )
+else:
+    STATIC_URL = '/static/'
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
