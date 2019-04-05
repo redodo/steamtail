cf17ca9001c7c3d8727e7b3b71d3d01af3dad4d1
redodo <me@redodo.io>
2019-04-05 13:17:40 +0200

    Fix settings

--- proj/settings/default.py
+++ proj/settings/default.py
@@ -53,14 +53,12 @@ if 'USE_S3_STATICFILES' in os.environ:
     STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
     # DigitalOcean-specific config
+    AWS_ACCESS_KEY_ID = secret_from_env('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = secret_from_env('AWS_SECRET_ACCESS_KEY')
 
     # TODO: make these configurable
     AWS_S3_REGION_NAME = 'ams3'
     AWS_STORAGE_BUCKET_NAME = 'steamtail'
-
-    AWS_S3_ENDPOINT_URL = '%s.digitaloceanspaces.com' % AWS_S3_REGION_NAME
-    AWS_ACCESS_KEY_ID = secret_from_env('AWS_ACCESS_KEY_ID')
-    AWS_SECRET_ACCESS_KEY = secret_from_env('AWS_SECRET_ACCESS_KEY')
 
     AWS_S3_ENDPOINT_URL = 'https://%s.%s.digitaloceanspaces.com' % (
         AWS_STORAGE_BUCKET_NAME,
