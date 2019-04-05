70c762a8a03caf143654020f891fa303146ceaba
redodo <me@redodo.io>
2019-04-05 11:59:58 +0200

    Fix secret fetching from file

--- proj/utils.py
+++ proj/utils.py
@@ -7,8 +7,10 @@ def secret_from_env(key, default=None):
     if secret is not None:
         return secret
 
-    secret_file = Path('%s_FILE' % key)
-    if secret_file.exists():
-        return secret_file.read_text()
+    secret_filename = os.environ.get('%s_FILE' % key)
+    if secret_filename:
+        secret_file = Path(secret_filename)
+        if secret_file.exists():
+            return secret_file.read_text()
 
     return default
